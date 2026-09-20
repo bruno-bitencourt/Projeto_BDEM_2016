640181af6f5aaaec59d71bb0f7ff9796b8823bf0
Brunelso <bitencourt.mamute@gmail.com>
2026-09-20 15:45:48 -0300

    script BDEM - SINASC - tarefas 1 a 7

--- script_BDEM.R
+++ script_BDEM.R
@@ -335,6 +335,12 @@ dados_sinasc_2$KOTELCHUCK <- factor(dados_sinasc_2$KOTELCHUCK, levels = c(1, 2, 
 # nova variável: dados_sinasc_2$ESTCIV: Sem companheiro: ESTCIVMAE 1, 3 ou 4, Com companheiro: ESTCIVMAE 2 ou 5
 # Ao categorizar as variáveis, garantir que sejam transformadas em tipo fator
 
+dados_sinasc_2$F_PESO <- cut(dados_sinasc_2$PESO, breaks = c(-Inf, 2500, 4000, Inf), labels = c("Baixo peso", "Peso normal", "Macrossomia"), right = FALSE)
+dados_sinasc_2$F_IDADE <- cut(dados_sinasc_2$IDADEMAE, breaks = c(-Inf, 15, 20, 25, 30, 35, 40, 45, 50, Inf), labels = c("<15", "15-19", "20-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50+"), right = FALSE)
+dados_sinasc_2$F_APGAR5 <- cut(dados_sinasc_2$APGAR5, breaks = c(-Inf, 7, Inf), labels = c("Baixo", "Normal"), right = FALSE)
+dados_sinasc_2$PEREG <- as.factor(ifelse(dados_sinasc_2$CODMUNNASC == dados_sinasc_2$CODMUNRES, "Não", "Sim"))
+dados_sinasc_2$ESTCIV <- as.factor(ifelse(dados_sinasc_2$ESTCIVMAE %in% c("Solteira", "Viúva", "Divorciada"), "Sem companheiro", ifelse(dados_sinasc_2$ESTCIVMAE %in% c("Casada", "União estável"), "Com companheiro", NA)))
+
 
 # Ao terminar a Tarefa 7 commit com a mensagem "script BDEM - SINASC - tarefas 1 a 7" e envie para o repositório Projeto_BDEM_2016
 
