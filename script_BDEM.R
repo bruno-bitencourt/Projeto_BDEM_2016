afe5d2b08bd48a283a01fae5e0e007f1d78e3889
Brunelso <bitencourt.mamute@gmail.com>
2026-09-20 14:45:27 -0300

    script BDEM - SINASC - tarefa 1

--- script_BDEM.R
+++ script_BDEM.R
@@ -219,6 +219,11 @@ write.csv(SIM_UF, file = "SIM_PA.csv", row.names = FALSE)
 # Por uma questão de padronização coloque todos os nomes das variáveis em letra maiúscula,
 # usando o comando names(dados_sinasc) = toupper(names(dados_sinasc))
 
+dados_sinasc <- read.csv2("SINASC_2016.csv")
+head(dados_sinasc)
+str(dados_sinasc)
+dim(dados_sinasc)
+names(dados_sinasc) <- toupper(names(dados_sinasc))
 
 # Ao terminar a Tarefa 1 commit com a mensagem "script BDEM - SINASC - tarefa 1" e envie para o repositório Projeto_BDEM_2016
 
